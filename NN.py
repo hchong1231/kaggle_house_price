305bc320c075d634dc7ce1c92398aa09d39966f1
hersch <hcc_public@126.com>
2017-10-15 17:22:45 +0800

    change

--- NN.py
+++ NN.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import tensorflow as tf
-from datasets import train, test, result, validation
+from datasets import train, test, result
 import numpy as np
 import pandas as pd
 class Config:
@@ -8,7 +8,7 @@ class Config:
     best Config={max_step: 4000,
                  batch_size: 64}
     """
-    max_step = 2300
+    max_step = 10000
     batch_size = 64
     learning_rate = 0.0001
     features_size = len(train.df.columns)
@@ -20,34 +20,27 @@ def save_result(data, columns):
     """
     index = []
     for i in range(len(data)):
-        index.append(i+892)
+        index.append(i+1461)
     data = np.column_stack((index, data))
     df = pd.DataFrame(data, columns=columns)
-    df = df.astype('int')
+    df['Id'] = df['Id'].astype('int')
     df.to_csv("result.csv", index=False)
     print()
 
-def do_eval(model, features, labels):
-    _acc, _loss = model.run([accuracy, loss], feed_dict={x_placeholder: features,
-                                                         y_placeholder: labels,
-                                                         train_phase: False})
-    print("Accuracy: %.5lf, Loss: %.5lf" %(_acc, _loss))
-
 # NN
 x_placeholder = tf.placeholder(tf.float32, shape=[None, Config.features_size])
-y_placeholder = tf.placeholder(tf.int32, shape=[None, ])
+y_placeholder = tf.placeholder(tf.float32, shape=[None, ])
 train_phase = tf.placeholder(tf.bool)
 hidden = x_placeholder
-hidden = tf.layers.dense(inputs=hidden, units=32, activation=tf.nn.relu)
+hidden = tf.layers.dense(inputs=hidden, units=128, activation=tf.nn.relu)
+hidden = tf.nn.dropout(hidden, keep_prob=0.8)
+hidden = tf.layers.dense(inputs=hidden, units=64, activation=tf.nn.relu)
 hidden = tf.nn.dropout(hidden, keep_prob=0.8)
 hidden = tf.layers.dense(inputs=hidden, units=32, activation=tf.nn.relu)
 hidden = tf.nn.dropout(hidden, keep_prob=0.8)
-logits = tf.layers.dense(inputs=hidden, units=2)
-y_one_hot = tf.one_hot(y_placeholder, depth=2)
+logits = tf.layers.dense(inputs=hidden, units=1)
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_one_hot, logits=logits))
-correct = tf.equal(tf.argmax(logits, 1), tf.argmax(y_one_hot, 1))
-accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(logits-y_placeholder)))
 train_op = tf.train.AdamOptimizer().minimize(loss)
 
 with tf.Session() as sess:
@@ -60,13 +53,12 @@ with tf.Session() as sess:
                                       train_phase: True})
         if step % 100 == 0:
             print("Step:%d " % step)
-            print("Train_set: ")
-            do_eval(sess, train.df, train.labels)
-            # print("Validation_set: ")
-            # do_eval(sess, validation.df, validation.labels)
             print("Test_set: ")
-            do_eval(sess, test.df, result)
+            _pre = logits.eval(feed_dict={x_placeholder: test.df, train_phase: False})
+            _pre = np.reshape(_pre, newshape=(-1,))
+            N = len(_pre)
+            print(np.c_[_pre, result['SalePrice']])
     _pre = sess.run(logits, feed_dict={x_placeholder: test.df,
                                        train_phase: False})
-    _pre = np.argmax(_pre, 1)
-    save_result(_pre, ['PassengerId', 'Survived'])
+    _pre = np.reshape(_pre, newshape=(-1,))
+    save_result(_pre, ['Id', 'SalePrice'])
--- datasets.py
+++ datasets.py
@@ -1,45 +1,32 @@
-# -*- coding: UTF-8 -*-
 import numpy as np
 import pandas as pd
-train_data = pd.read_csv("train.csv", header=0, delimiter=',')
-test_data = pd.read_csv("test.csv", header=0, delimiter=',')
-result = pd.read_csv("gender_submission.csv", header=0, delimiter=',')
-result = result['Survived']
+from sklearn import preprocessing
+train_data = pd.read_csv("train.csv", header=0)
+test_data = pd.read_csv("test.csv", header=0)
+result = pd.read_csv("sample_submission.csv", header=0)
 class Datasets:
-    def __init__(self, data, is_training):
-        self.df = self.preprocess(data)
-        if is_training:
-            #self.df = self.oversampling(self.df)
-            self.labels = self.df['Survived']
-            self.df = self.df.iloc[:, 1:]
-        self.num_examples = len(self.df)
+    def __init__(self, data, train_flag):
         self.pos = 0
-    def oversampling(self, data):
-        pos = data[data['Survived'] == 1]
-        neg = data[data['Survived'] == 0]
-        n_pos = len(pos)
-        n_neg = len(neg)
-        if n_pos > n_neg:
-            neg = pd.concat([neg, neg.sample(n=n_pos-n_neg, replace=True)])
-        else:
-            pos = pd.concat([pos, pos.sample(n=n_neg - n_pos, replace=True)])
-        data = pd.concat([neg, pos])
-        return data
+        self.num_examples = len(data)
+        self.df = data
+        if train_flag:
+            self.labels = data['SalePrice']
+            self.df = data.iloc[:, :-1]
+        self.df = self.preprocess(self.df)
     def preprocess(self, data):
-        data = data.drop(labels=['Cabin', 'PassengerId', 'Ticket', 'Name'], axis=1)
-        data['Sex'][data['Sex'] == 'male'] = 0
-        data['Sex'][data['Sex'] == 'female'] = 1
-        data['Fare'] = data['Fare'].fillna(data['Fare'].median())
-        data['Fare'] = data['Fare'].apply(lambda x: (x-data['Fare'].min())/(data['Fare'].max()- data['Fare'].min()))
-        # data format
-        data = pd.concat([data, pd.get_dummies(data['Embarked'], prefix='Embarked')], axis=1)
-        data = pd.concat([data, pd.get_dummies(data['Pclass'], prefix='Pclass')], axis=1)
-        data = data.drop(labels=['Embarked', 'Pclass'], axis=1)
-        # fill null nan
+        # drop columns
+        data = data.drop(['Alley', 'PoolQC', 'Fence', 'MiscFeature', 'FireplaceQu',
+                        'LotFrontage', 'GarageCond', 'GarageType', 'GarageYrBlt',
+                        'GarageQual', 'BsmtExposure', 'BsmtFinType1', 'BsmtCond',
+                          'MasVnrArea'], axis=1)
+        # transfer string -> interger
         for x in data.columns:
-            data[x][data[x].isnull()] = data[x].median()
-        data['Age'][data['Age'] < 15.0] = 0
-        data['Age'][data['Age'] >= 15.0] = 1
+            if data[x].dtype == object:
+                data = pd.concat((data, pd.get_dummies(data[x], prefix=x)), axis=1)
+                del data[x]
+            else:
+                data[x] = data[x].fillna(data[x].median())
+                data[x] = data[x].apply(lambda m: (m-data[x].mean())/data[x].std())
         return data
     def next_batch(self, batch_size):
         features = self.df.iloc[self.pos:self.pos+batch_size, :]
@@ -48,17 +35,11 @@ class Datasets:
         if self.pos >= self.num_examples:
             self.pos = 0
         return features, labels
-# delete features of train dataset which is not in test dataset
-def train_test_feature_equal(train, test):
-    for x in train.columns:
-        if x in test.columns:
-            pass
-        else:
-            if x != 'Survived':
-                train = train.drop(x, axis=1)
-    return train, test
-train_div = int(len(train_data)*0.2)
-train = Datasets(train_data.iloc[:, :], True)
-validation = Datasets(train_data.iloc[:train_div, :], True)
+train = Datasets(train_data, True)
 test = Datasets(test_data, False)
-train.df, test.df = train_test_feature_equal(train.df, test.df)
+for x in train.df.columns:
+    if x in test.df.columns:
+        pass
+    else:
+        train.df = train.df.drop(x, axis=1)
+print(len(train.df.columns), len(test.df.columns))
